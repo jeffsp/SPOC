a50eea34c552728ac7df47df192ec109a06f3011
Jeff Perry <jeffsp@gmail.com>
2022-01-22 16:25:05 -0600

    Undo last change -- don't print default SRS

--- convert_one.sh
+++ convert_one.sh
@@ -31,8 +31,8 @@ if grep -q PROJCS ${TMPDIR}/info.txt; then
     # Save the projection
     grep PROJCS ${TMPDIR}/info.txt > $2/${sn}.txt
 else
-    # If there is no projection, use a default (US National Mall)
-    echo "    COMPD_CS["unknown",PROJCS["WGS 84 / UTM zone 18N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-75],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","32618"]],VERT_CS["unknown",VERT_DATUM["World Geodetic System 1984",2002,AUTHORITY["EPSG","6326"]],UNIT["metre",1.0,AUTHORITY["EPSG","9001"]],AXIS["Up",UP]]]" > $2/${sn}.txt
+    # If there is no projection, print a blank line
+    echo "" > $2/${sn}.txt
 fi
 
 # Append the data to the file
