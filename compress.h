13562400cfcaaf55d76be9cc47ca77d1de9843b7
Jeff Perry <jeffsp@gmail.com>
2022-02-13 18:54:27 -0600

    Simplify vector compress function

--- compress.h
+++ compress.h
@@ -70,37 +70,29 @@ struct Inflator
 
 inline std::vector<uint8_t> compress (const std::vector<uint8_t> &input, const int level)
 {
-    size_t input_index = 0;
     constexpr size_t BUFFER_SIZE = (1 << 20);
     std::vector<uint8_t> output_buffer (BUFFER_SIZE);
     std::vector<uint8_t> output;
 
     Deflator deflator (level);
 
-    do {
         // Get bytes directly from the input vector
-        size_t bytes_left = input.size () - input_index;
-        const bool last_buffer = bytes_left <= BUFFER_SIZE;
-        deflator.s.avail_in = last_buffer ? bytes_left : BUFFER_SIZE;
-        deflator.s.next_in = const_cast<unsigned char *> (&input[input_index]);
+        deflator.s.avail_in = input.size ();
+        deflator.s.next_in = const_cast<unsigned char *> (&input[0]);
 
-        do {
-            // Compress from input to output buffer
-            deflator.s.avail_out = BUFFER_SIZE;
-            deflator.s.next_out = &output_buffer[0];
-            deflate (&deflator.s, last_buffer ? Z_FINISH : Z_NO_FLUSH);
-            const auto compressed_bytes = BUFFER_SIZE - deflator.s.avail_out;
+    do {
+        // Compress from input to output buffer
+        deflator.s.avail_out = BUFFER_SIZE;
+        deflator.s.next_out = &output_buffer[0];
+        deflate (&deflator.s, Z_FINISH);
+        const auto compressed_bytes = BUFFER_SIZE - deflator.s.avail_out;
 
-            // Save compressed bytes
-            assert (compressed_bytes <= output_buffer.size ());
-            output.insert (output.end (),
-                output_buffer.begin (),
-                output_buffer.begin () + compressed_bytes);
-        } while (deflator.s.avail_out == 0);
-
-        // Go to next index in input
-        input_index += BUFFER_SIZE;
-    } while (input_index < input.size ());
+        // Save compressed bytes
+        assert (compressed_bytes <= output_buffer.size ());
+        output.insert (output.end (),
+            output_buffer.begin (),
+            output_buffer.begin () + compressed_bytes);
+    } while (deflator.s.avail_out == 0);
 
     return output;
 }
