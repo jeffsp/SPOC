cfff5c3af0113d772ba4889ddae0477139362379
Jeff Perry <jeffsp@gmail.com>
2022-04-01 17:24:40 -0500

    Don't create tmpdirs if you don't need them

--- convert_laz_one.sh
+++ convert_laz_one.sh
@@ -6,20 +6,8 @@ dn=$(dirname $1)
 sn=$(basename ${dn})
 echo converting ${sn}...
 
-# Create a tmp directory for intermediate files
-TMPDIR=$(mktemp --tmpdir --directory spoc.XXXXXXXX)
-
-# Create a cleanup function
-function cleanup {
-    rm -rf ${TMPDIR}
-}
-
-# Run cleanup on exit
-trap cleanup EXIT
-
 # Convert to las
 laszip64 $1 -o $2/${sn}.las
 
 # Convert to laz
 laszip64 $1 -o $2/${sn}.laz
-
--- convert_spoc_one.sh
+++ convert_spoc_one.sh
@@ -6,16 +6,5 @@ dn=$(dirname $1)
 sn=$(basename ${dn})
 echo converting ${sn}...
 
-# Create a tmp directory for intermediate files
-TMPDIR=$(mktemp --tmpdir --directory spoc.XXXXXXXX)
-
-# Create a cleanup function
-function cleanup {
-    rm -rf ${TMPDIR}
-}
-
-# Run cleanup on exit
-trap cleanup EXIT
-
 # Convert to spoc
 ./build/release/text2spoc -v < $2/${sn}.txt > $2/${sn}.spoc
