3cdad9dbf22e21a78d584b13bb5d6f3454a60632
Jeff Perry <jeffsp@gmail.com>
2022-04-03 16:55:27 -0500

    Write gnu parallel joblog

--- convert_spoc_all.sh
+++ convert_spoc_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 # Convert to spoc file
-parallel -k ./convert_spoc_one.sh {} $2 ::: \
+parallel -k --joblog /tmp/${0}.log --jobs 3 ./convert_spoc_one.sh {} $2 ::: \
 $(find $1 -name "*.las" -or -name "*.laz")
