3c0b2825b91a575461be9b9746933bf58c35d37e
Jeff Perry <jeffsp@gmail.com>
2022-04-13 20:24:20 -0500

    Begin adding spoc_info

--- apps/spoc_info.cpp
+++ apps/spoc_info.cpp
@@ -2,8 +2,33 @@
 #include "spoc_info_cmd.h"
 #include "json.h"
 #include <iostream>
+#include <map>
 #include <stdexcept>
 #include <string>
+
+template<typename T,typename U>
+std::map<std::string,T> get_summary (const U &x)
+{
+    std::map<std::string,T> m;
+    if (x.empty ())
+    {
+        m.insert ({"size", 0});
+        m.insert ({"min", 0});
+        m.insert ({"max", 0});
+        return m;
+    }
+    // Copy
+    auto y (x);
+    const size_t n = y.size ();
+    std::sort (begin (y), end (y));
+    m.insert ({"range", y.back() - y.front()});
+    m.insert ({"q0", y.front()});
+    m.insert ({"q1", y[n / 4]});
+    m.insert ({"q2", y[n / 2]});
+    m.insert ({"q3", y[3 * n / 4]});
+    m.insert ({"q4", y.back()});
+    return m;
+}
 
 int main (int argc, char **argv)
 {
@@ -45,26 +70,39 @@ int main (int argc, char **argv)
             j["header"]["npoints"] = f.get_npoints ();
         }
 
-        for (size_t i = 0; i < f.get_npoints(); ++i)
+        if (args.summary_info)
         {
-            // Get the point record
-            const auto p = f.get (i);
+            for (size_t i = 0; i < f.get_npoints(); ++i)
+            {
+                // Get the point record
+                const auto p = f.get (i);
+            }
+
+            j["summary"]["x"] = get_summary<double> (f.get_x ());
+            j["summary"]["y"] = get_summary<double> (f.get_y ());
+            j["summary"]["z"] = get_summary<double> (f.get_z ());
+            j["summary"]["c"] = get_summary<uint16_t> (f.get_c ());
+            j["summary"]["p"] = get_summary<uint16_t> (f.get_p ());
+            j["summary"]["i"] = get_summary<uint16_t> (f.get_i ());
+            j["summary"]["r"] = get_summary<uint16_t> (f.get_r ());
+            j["summary"]["g"] = get_summary<uint16_t> (f.get_g ());
+            j["summary"]["b"] = get_summary<uint16_t> (f.get_b ());
+            for (size_t k = 0; k < f.get_extra ().size (); ++k)
+                j["summary"]["extra"][std::to_string(k)] = get_summary<uint64_t> (f.get_extra ()[k]);
         }
 
-        j["points"]["x"] = f.get_x ().size ();
-        j["points"]["y"] = f.get_y ().size ();
-        j["points"]["z"] = f.get_z ().size ();
-
         if (args.json)
+        {
             cout << j.dump(4) << endl;
+        }
         else
         {
-            for (auto i = j.begin(); i != j.end(); ++i)
+            for (auto i1 = j.begin(); i1 != j.end(); ++i1)
             {
-                cout << i.key() << endl;
-                for (auto k = i->begin(); k != i->end(); ++k)
+                cout << i1.key() << endl;
+                for (auto i2 = i1->begin(); i2 != i1->end(); ++i2)
                 {
-                    cout << "\t" << k.key() << "\t" << k.value() << endl;
+                    cout << "\t" << i2.key() << "\t" << i2.value() << endl;
                 }
             }
         }
