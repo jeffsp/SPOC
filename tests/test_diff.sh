4a2080a66acb9516bf1a3be2e1717e4b0585d194
Jeff Perry <jeffsp@gmail.com>
2022-04-27 21:24:25 -0500

    Turn off verbose output in test

--- tests/test_diff.sh
+++ tests/test_diff.sh
@@ -4,4 +4,4 @@
 set -e
 
 spoc_diff --help 2> /dev/null
-spoc_diff --verbose ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
