cb21a7cb34889c0052d71ab8dbe298223e8f277e
Jeff Perry <jeffsp@gmail.com>
2022-04-28 18:04:14 -0500

    Add more spoc_diff integration tests

--- tests/test_diff.sh
+++ tests/test_diff.sh
@@ -5,3 +5,12 @@ set -e
 
 spoc_diff --help 2> /dev/null
 spoc_diff ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -e ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -d ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -f x ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -f xyzrgbpci01234567 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -f x --field=yz --field=rgbpci01234567 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+# Invalid field specifier
+! spoc_diff -f q ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc 2> /dev/null
+# Invalid field specifier
+! spoc_diff -f xyz --field=012345678 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc 2> /dev/null
