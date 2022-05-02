ebd3221ceb8ff75a4c3dcf61faff08901e34cf96
Jeff Perry <jeffsp@gmail.com>
2022-05-01 19:58:32 -0500

    Add transform tests

--- tests/test_transform.cpp
+++ tests/test_transform.cpp
@@ -6,8 +6,68 @@
 using namespace std;
 using namespace spoc::transform;
 
-void test_transform ()
+void test_transform_set ()
 {
+    for (auto rgb : {true, false})
+    {
+        // Generate some records
+        const auto p = get_random_point_records (100, rgb);
+
+        // Generate a spoc_file
+        stringstream s;
+        write_spoc_file (s, string ("Test WKT"), p);
+        auto f = spoc::read_spoc_file (s);
+
+        for (auto c : { 'x', 'y', 'z',
+                'c', 'p', 'i', 'r', 'g', 'b',
+                '0', '1', '2', '3', '4', '5', '6', '7' })
+            const auto g = run_set_command (f, c, 123);
+    }
+}
+
+void test_transform_replace ()
+{
+    for (auto rgb : {true, false})
+    {
+        // Generate some records
+        const auto p = get_random_point_records (100, rgb);
+
+        // Generate a spoc_file
+        stringstream s;
+        write_spoc_file (s, string ("Test WKT"), p);
+        auto f = spoc::read_spoc_file (s);
+
+        vector<pair<unsigned,unsigned>> r;
+        for (auto r1 : { 1, 2, 3, 4, 5 })
+            for (auto r2 : { 1, 2, 3, 4, 5 })
+                r.push_back (make_pair (r1, r2));
+        for (auto c : { 'c', 'p', 'i', 'r', 'g', 'b',
+                '0', '1', '2', '3', '4', '5', '6', '7' })
+            const auto g = run_replace_command (f, c, r);
+        for (auto c : { 'x', 'y', 'z', 'q', 'w'})
+        {
+            bool failed = false;
+            try { const auto g = run_replace_command (f, c, r); }
+            catch (...) { failed = true; }
+            verify (failed);
+        }
+    }
+}
+
+void test_transform_pipes ()
+{
+    for (auto rgb : {true, false})
+    {
+        // Generate some records
+        const auto p = get_random_point_records (100, rgb);
+
+        // Generate a spoc_file
+        stringstream s;
+        write_spoc_file (s, string ("Test WKT"), p);
+        auto f = spoc::read_spoc_file (s);
+        stringstream ss;
+        auto g = run_pipe_command (f, ss, ss);
+    }
 }
 
 int main (int argc, char **argv)
@@ -15,7 +75,9 @@ int main (int argc, char **argv)
     using namespace std;
     try
     {
-        test_transform ();
+        test_transform_set ();
+        test_transform_replace ();
+        test_transform_pipes ();
         return 0;
     }
     catch (const exception &e)
