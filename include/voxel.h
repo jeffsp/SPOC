b6096f2aa9c06ce57121aa2d30814754441d688e
Jeff Perry <jeffsp@gmail.com>
2022-05-09 14:03:28 -0500

    Add voxel support

---
+++ include/voxel.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "extent.h"
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+namespace spoc
+{
+
+namespace voxel
+{
+
+// A 3D index into a packed 3D volume
+struct voxel_index
+{
+    voxel_index ()
+        : i (0)
+        , j (0)
+        , k (0)
+    {}
+
+    voxel_index (size_t i, size_t j, size_t k)
+        : i (i)
+        , j (j)
+        , k (k)
+    {}
+
+    size_t i;
+    size_t j;
+    size_t k;
+    bool operator== (const voxel_index &v) const
+    {
+        return v.i == i && v.j == j && v.k == k;
+    }
+};
+
+// Get a 3D index given a point in xyz space
+template<typename T>
+voxel_index get_voxel_index (const T &p, const T &minp, const double res)
+{
+    voxel_index v;
+    v.i = (p.x - minp.x) / res;
+    v.j = (p.y - minp.y) / res;
+    v.k = (p.z - minp.z) / res;
+    return v;
+}
+
+template<typename T>
+std::vector<voxel_index> get_voxel_indexes (const T &points, const double res)
+{
+    // Return value
+    std::vector<voxel_index> voxel_indexes (points.size ());
+    const auto e = get_extent (points);
+#pragma omp parallel for
+    for (size_t i = 0; i < voxel_indexes.size (); ++i)
+        voxel_indexes[i] = get_voxel_index (points[i], e.minp, res);
+    return voxel_indexes;
+}
+
+// Compute a hash value from a voxel index.
+struct voxel_index_hash
+{
+    std::size_t operator() (const voxel_index &v) const
+    {
+        size_t hash = (v.i << 0) ^ (v.j << 16) ^ (v.k << 32);
+        return hash;
+    }
+};
+
+// Typedefs for maps and sets of voxel indexes
+using voxel_index_set = std::unordered_set<voxel_index, voxel_index_hash>;
+using voxel_index_map = std::unordered_map<voxel_index, std::vector<size_t>, voxel_index_hash>;
+
+template<typename T>
+voxel_index_set get_voxel_index_set (const T &voxel_indexes)
+{
+    voxel_index_set vis;
+    vis.insert (voxel_indexes.begin (), voxel_indexes.end ());
+    return vis;
+}
+
+template<typename T>
+voxel_index_map get_voxel_index_map (const T &voxel_indexes)
+{
+    voxel_index_map vim;
+    for (size_t i = 0; i < voxel_indexes.size (); ++i)
+    {
+        voxel_index v = voxel_indexes[i];
+        vim[v].push_back (i);
+    }
+    // Free unused memory
+    for (auto &v : vim)
+        v.second.shrink_to_fit ();
+    return vim;
+}
+
+
+} // namespace voxel
+
+} // namespace spoc
---
+++ tests/test_voxel.cpp
@@ -0,0 +1,135 @@
+#include "test_utils.h"
+#include "voxel.h"
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+using namespace spoc;
+using namespace spoc::voxel;
+
+using P = std::vector<point<double>>;
+
+void test_empty ()
+{
+    P pc;
+    auto indexes = get_voxel_indexes (pc, 1.0);
+    verify (indexes.empty());
+}
+
+void test_single ()
+{
+    P pc;
+    pc.push_back({0, 0, 0});
+    auto v = get_voxel_indexes (pc, 1.0);
+    verify(v[0].i == 0);
+    verify(v[0].j == 0);
+    verify(v[0].k == 0);
+}
+
+void test_res ()
+{
+    P pc;
+    pc.push_back({0, 0, 0});
+    pc.push_back({4, 1, 1});
+    auto v = get_voxel_indexes(pc, 1.5);
+    verify(v[1].i == 2);
+    verify(v[1].j == 0);
+    verify(v[1].k == 0);
+}
+
+void test_neg ()
+{
+    P pc;
+    pc.push_back({-1, -1, -1});
+    pc.push_back({1, 1, 1});
+    auto v = get_voxel_indexes (pc, 1.0);
+    verify(v[1].i == 2);
+    verify(v[1].j == 2);
+    verify(v[1].k == 2);
+}
+
+void test_multiple ()
+{
+    P pc;
+    pc.push_back({0, 0, 0});
+    pc.push_back({1, 1, 1});
+    pc.push_back({0, 1, 2});
+    auto v = get_voxel_indexes (pc, 1.0);
+    verify(v.at(0).i == 0 && v.at(0).j == 0 && v.at(0).k == 0);
+    verify(v.at(1).i == 1 && v.at(1).j == 1 && v.at(1).k == 1);
+    verify(v.at(2).i == 0 && v.at(2).j == 1 && v.at(2).k == 2);
+}
+
+void test_vis ()
+{
+    P pc;
+    pc.push_back({0, 1, 2});
+    pc.push_back({3, 4, 5});
+    pc.push_back({6, 7, 8});
+    auto v1 = get_voxel_indexes (pc, 2.1); // {0,0,0} {1,1,1} {2,2,2}
+    auto v2 = get_voxel_indexes (pc, 0.99); // {0,0,0} {3,3,3} {6,6,6}
+    auto vis1 = get_voxel_index_set (v1);
+    auto vis2 = get_voxel_index_set (v2);
+    verify (vis1.size () == 3);
+    verify (vis2.size () == 3);
+    verify (vis1.find ({0, 0, 0}) != vis1.end ());
+    verify (vis1.find ({1, 1, 1}) != vis1.end ());
+    verify (vis1.find ({2, 2, 2}) != vis1.end ());
+    verify (vis1.find ({1, 2, 1}) == vis1.end ());
+    verify (vis2.find ({0, 0, 0}) != vis2.end ());
+    verify (vis2.find ({3, 3, 3}) != vis2.end ());
+    verify (vis2.find ({6, 6, 6}) != vis2.end ());
+    verify (vis2.find ({0, 0, 3}) == vis2.end ());
+}
+
+void test_vim ()
+{
+    P pc;
+    pc.push_back({0, 0, 0});
+    pc.push_back({1, 1, 1});
+    pc.push_back({2, 2, 2});
+    auto v1 = get_voxel_indexes (pc, 1.5); // {0,0,0} {0,0,0} {1,1,1}
+    auto v2 = get_voxel_indexes (pc, 0.99); // {0,0,0} {1,1,1} {2,2,2}
+    auto vim1 = get_voxel_index_map (v1);
+    auto vim2 = get_voxel_index_map (v2);
+    verify(vim1.at ({0, 0, 0}).size() == 2);
+    verify(vim2.at ({1, 1, 1}).size() == 1);
+    for (auto &&i : vim1)
+    {
+        clog << i.first.i;
+        clog << '\t' << i.first.j;
+        clog << '\t' << i.first.k;
+        clog << endl;
+    }
+    verify (vim1.size () == 2);
+    verify (vim2.size () == 3);
+    verify (vim1.find ({0, 0, 0}) != vim1.end ());
+    verify (vim1.find ({1, 1, 1}) != vim1.end ());
+    verify (vim1.find ({2, 2, 2}) == vim1.end ());
+    verify (vim1.find ({1, 2, 1}) == vim1.end ());
+    verify (vim2.find ({0, 0, 0}) != vim2.end ());
+    verify (vim2.find ({1, 1, 1}) != vim2.end ());
+    verify (vim2.find ({2, 2, 2}) != vim2.end ());
+    verify (vim2.find ({0, 0, 3}) == vim2.end ());
+}
+
+int main ()
+{
+    try
+    {
+        test_empty ();
+        test_single ();
+        test_res ();
+        test_neg ();
+        test_multiple ();
+        test_vis ();
+        test_vim ();
+
+        return 0;
+    }
+    catch (const exception &e)
+    {
+        cerr << "exception: " << e.what () << endl;
+    }
+    return -1;
+}
