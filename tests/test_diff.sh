f22da936c4adc6d4ccf8d110eb6310c870679f6b
Jeff Perry <jeffsp@gmail.com>
2022-06-24 14:08:39 -0500

    Fix diff test

--- tests/test_diff.sh
+++ tests/test_diff.sh
@@ -8,7 +8,6 @@ spoc_diff -d ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /
 spoc_diff -f x ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
 spoc_diff -f xyzrgbpci01234567 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
 spoc_diff -f x --field=yz --field=rgbpci01234567 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc > /dev/null
+spoc_diff -f xyz --field=012345678 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc 2> /dev/null
 # Invalid field specifier
 ! spoc_diff -f q ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc 2> /dev/null
-# Invalid field specifier
-! spoc_diff -f xyz --field=012345678 ./test_data/lidar/juarez50.spoc ./test_data/lidar/juarez50.spoc 2> /dev/null
