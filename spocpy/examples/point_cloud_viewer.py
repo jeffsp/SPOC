ba05d8c5fce8660bb25b7b23f568ed5fc622431d
Jeff Perry <jeffsp@gmail.com>
2022-10-27 10:23:51 -0500

    Update point cloud viewer example

--- spocpy/examples/point_cloud_viewer.py
+++ spocpy/examples/point_cloud_viewer.py
@@ -3,41 +3,83 @@
 Spocpy example
 """
 
+from dash import Dash, dcc, html, Input, Output
+
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objs as go
 import spocpy as sp
 
 
-def main():
+# Read a file
+fn = '../../test_data/eo/romeo007.spoc'
+f = sp.readspocfile(fn)
 
-    # Read a file
-    fn = '../../test_data/eo/romeo007.spoc'
-    f = sp.readspocfile(fn)
+# Create a Pandas dataframe
+df = pd.DataFrame()
 
-    # Create a Pandas dataframe
-    df = pd.DataFrame()
+# Add columns
+df["x"] = f.getX()
+df["y"] = f.getY()
+df["z"] = f.getZ()
 
-    # Add columns
-    df["x"] = f.getX()
-    df["y"] = f.getY()
-    df["z"] = f.getZ()
-    df["r"] = f.getR()
-    df["g"] = f.getG()
-    df["b"] = f.getB()
+# Compute colors
+colors = [f'rgb({r//256},{g//256},{b//256})'
+          for r, g, b in zip(f.getR(), f.getG(), f.getB())]
 
-    # Plot it
-    colors = [f'rgb({r//256},{g//256},{b//256})'
-              for r, g, b in zip(df.r.values, df.g.values, df.b.values)]
-    trace = go.Scatter3d(x=df.x, y=df.y, z=df.z,
-                         mode='markers', marker=dict(size=2, color=colors))
-    data = [trace]
-    layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
-    fig = go.Figure(data=data, layout=layout)
-    fig['layout']['scene']['aspectmode'] = "data"
-    fig.show()
+df["rgb"] = colors
+
+# Create figure
+fig = px.scatter_3d(data_frame=df, x='x', y='y', z='z')
+fig.update_layout(template='simple_white')
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+fig.update_scenes(aspectmode='data')
+fig.update_traces(marker=dict(size=2, color=colors))
+
+# Create the app
+app = Dash(__name__)
+
+# Create the layout
+app.layout = html.Div(
+    id='body',
+    style={
+        'display': 'flex',
+        'width': '99vw',
+        'height': '95vh',
+        },
+    children=[
+        html.Div(
+            id='controls',
+            style={
+                'display': 'flex',
+                'background-color': 'rgb(245,245,245)',
+                'flex-direction': 'row',
+                },
+            children=[
+                html.Div(dcc.Slider(0, 100, 10, id='slider1', vertical=True)),
+                html.Div(dcc.Slider(0, 100, 10, id='slider2', vertical=True)),
+                html.Div(dcc.Slider(0, 100, 10, id='slider3', vertical=True)),
+                html.Div(dcc.Slider(0, 100, 10, id='slider4', vertical=True)),
+                ]
+            ),
+        html.Div(
+            id="point-cloud",
+            style={
+                'background-color': 'rgb(0,255,0)',
+                'width': '100%',
+                'height': '100%',
+                },
+            children=[
+                    dcc.Graph(
+                        figure=fig,
+                        style={
+                            'width': '100%',
+                            'height': '100%',
+                            },
+                        ),
+                ],
+            )
+        ]
+    )
 
 
-if __name__ == "__main__":
-
-    main()
+app.run_server(debug=True)
