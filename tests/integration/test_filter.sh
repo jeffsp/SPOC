c2d35a87618847064847e062fe34c8c8f4a30292
Jeff Perry <jeffsp@gmail.com>
2024-04-29 14:30:23 -0500

    Cleanup temp files in integration test

--- tests/integration/test_filter.sh
+++ tests/integration/test_filter.sh
@@ -1,6 +1,17 @@
 # Fail on error
 set -e
 
+# Create a tmp directory for intermediate files
+TMPDIR=$(mktemp --tmpdir --directory spoc.XXXXXXXX)
+
+# Create a cleanup function
+function cleanup {
+    rm -rf ${TMPDIR}
+}
+
+# Run cleanup on exit
+trap cleanup EXIT
+
 spoc_filter --help 2> /dev/null
 
 spoc_filter --unique-xyz \
